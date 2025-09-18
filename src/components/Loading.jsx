1baf7fa8d18364fe6aad23e59c362c40954eed09
noor <sosonono20032018@gmail.com>
2025-09-18 19:37:32 +0300

    Initial commit

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,25 +1,9 @@
-import React, {useEffect, useRef} from 'react';
+import React from 'react';
 
 export default function Loading(){
-  const container = useRef(null);
-
-  useEffect(() => {
-    const animationUrl = "https://assets9.lottiefiles.com/packages/lf20_touohxv0.json";
-    if(window.lottie && container.current){
-      window.lottie.loadAnimation({
-        container: container.current,
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        path: animationUrl
-      });
-    }
-  }, []);
-
   return (
     <div className="loading-center">
-      <div ref={container} style={{width:320, height:320}}></div>
-      <h4 className="mt-3">Loading The learning platform...</h4>
+      <h4 className="loading-text">Loading...</h4>
     </div>
   );
 }
